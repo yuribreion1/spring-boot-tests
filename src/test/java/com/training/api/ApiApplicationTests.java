7220586adcc941f519767320b16d052159a2f882
Yuri Breion <yuribreion@gmail.com>
2023-08-19 11:52:09 -0300

    Adding first test on main

--- src/test/java/com/training/api/ApiApplicationTests.java
+++ src/test/java/com/training/api/ApiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.training.api;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    @DisplayName("Main class test")
+    void main() {
+        ApiApplication.main(new String[] {});
     }
 
 }
