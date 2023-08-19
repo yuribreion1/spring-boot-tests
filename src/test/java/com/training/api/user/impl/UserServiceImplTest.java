87b90a968300cb8ae14d877210de70cfff79d85f
Yuri Breion <yuribreion@Yuris-Mac-mini.local>
2023-08-19 15:40:17 -0300

    Adding test

--- src/test/java/com/training/api/user/impl/UserServiceImplTest.java
+++ src/test/java/com/training/api/user/impl/UserServiceImplTest.java
@@ -1,20 +1,23 @@
 package com.training.api.user.impl;
 
+import com.training.api.exception.ObjectNotFoundException;
 import com.training.api.user.User;
 import com.training.api.user.UserRepository;
 import com.training.api.user.dto.UserDTO;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -23,12 +26,16 @@ class UserServiceImplTest {
     public static final String NAME = "Yuri";
     public static final String MAIL = "yuri@mail.com";
     public static final String PASSWORD = "1234";
+
+    // Inject service implementation using the real methods and data
     @InjectMocks
     UserServiceImpl userService;
 
+    // Mock annotation enable the repository using mock data
     @Mock
     UserRepository userRepository;
 
+    // Mock annotation enable the repository using mock data
     @Mock
     ModelMapper modelMapper;
 
@@ -48,13 +55,23 @@ class UserServiceImplTest {
     @Test
     @DisplayName("Testing findById method")
     void whenFindByIdThenReturnsAnUserInstance() {
-        Mockito
-                .when(userRepository
-                        .findById(Mockito
-                                .anyInt()))
+                when(userRepository
+                        .findById(anyInt()))
                 .thenReturn(optionalUser);
         User userResponse = userService.findById(ID);
-        Assertions.assertEquals(User.class, userResponse.getClass());
+        assertEquals(User.class, userResponse.getClass());
+    }
+
+    @Test
+    @DisplayName("Testing findById not found")
+    void whenFindByIdThenReturnObjectNotFoundException() {
+        when(userRepository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Object not found"));
+
+        try{
+            userService.findById(ID);
+        } catch (Exception e) {
+            assertEquals("Object not found", e.getMessage());
+        }
     }
 
     @Test
